8e979feee8f1bce1c08825c3a73e977d454e83e2
veetmoradiya3628 <veetmoradiya3628@gmail.com>
2024-11-17 13:04:23 +0530

    go q&a addded

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### Excersize questions or QnA for GO
+
+https://endurable-treatment-735.notion.site/Go-Lang-1360e2bd80ec80b991cfc30539352f52
--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
-import "fmt"
 
+import ("fmt"; "math")
+
+type Shape interface {
+	perimeter() float64
+}
+func (c* Cicle) perimeter() float64 {
+	return 2 * math.Pi * c.r
+}
+func (r* Ractangle) perimeter() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return 2 * (l + w)
+}
 func main() {
-  fmt.Println("Hello world!")
+	xs := [5]int{1, 2, 5, 4, 3}
+	fmt.Println(maxVal(xs))
 }
